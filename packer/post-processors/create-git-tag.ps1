54144d44237b3a803db7c97dfeb0f5c4b9678e9f
Urs Voegele <unext@bluewin.ch>
2021-03-05 15:00:50 +0100

    Vagrantfile fpr download fixed

--- packer/post-processors/create-git-tag.ps1
+++ packer/post-processors/create-git-tag.ps1
@@ -23,6 +23,7 @@ Vagrant.configure("2") do |config|
       vb.customize ["modifyvm", :id, "--memory", "4096" ]
       vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
       vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+      vb.customize ["modifyvm", :id, "--uartmode1", "disconnected" ]
       vb.customize ["modifyvm", :id, "--groups", "/#{$vb_group_name}" ]
       vb.customize ["modifyvm", :id, "--vram", 256 ]
     end
